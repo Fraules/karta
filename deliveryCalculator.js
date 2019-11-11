3b467893e103c5ff8b963e709a58d2fd15fcebd9
Olesya Pavlovna <Fraules@users.noreply.github.com>
2019-11-11 10:20:23 +0300

    Update deliveryCalculator.js

--- deliveryCalculator.js
+++ deliveryCalculator.js
@@ -75,7 +75,25 @@ var Car_20_tonn= 52;
         // Зададим максимально допустимое число маршрутов, возвращаемых мультимаршрутизатором.
         route.model.setParams({results: 1}, true);
         
-
+  //
+        function round(value, exp) {
+            if (typeof exp === 'undefined' || +exp === 0)
+              return Math.round(value);
+          
+            value = +value;
+            exp = +exp;
+          
+            if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0))
+              return NaN;
+          
+            // Shift
+            value = value.toString().split('e');
+            value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp)));
+          
+            // Shift back
+            value = value.toString().split('e');
+            return +(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp));
+          }
         // Повесим обработчик на событие построения маршрута.
         route.model.events.add('requestsuccess', function () {
             var activeRoute = route.getActiveRoute();
